875eee3425c886d9b1c4e3011d56e8649b273e70
Baibhav Satapathy <72118471+Baibhav-source@users.noreply.github.com>
2022-12-22 21:04:53 +0530

    Create 278. First Bad Version.cpp

---
+++ Level 1/Day7/278. First Bad Version.cpp
@@ -0,0 +1,18 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int beg = 1,end = n;
+        while(beg <= end)
+        {
+            int mid = beg+(end-beg)/2;
+            if(isBadVersion(mid) == false) 
+                beg = mid+1;
+            else 
+                end = mid-1;
+        }
+        return beg;
+    }
+};
